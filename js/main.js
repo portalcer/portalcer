eeb1ea9ca509d89ee6ec7a585e4ce1fcfcf46482
portalcer <52223991+portalcer@users.noreply.github.com>
2019-08-24 23:45:09 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ jQuery(function($) {'use strict';
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
 		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+			form_status.html('<p class="text-success">Obrigado por nos contatar. O mais cedo possível entraremos em contato.</p>').delay(3000).fadeOut();
 		});
 	});
 
